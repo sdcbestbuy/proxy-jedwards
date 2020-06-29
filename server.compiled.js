35580303db11e25ac2af6075ea0a71f9a633e391
Benjamin Polancich <polancichben@gmail.com>
2020-06-29 10:59:31 -0500

    New compiled Server

--- server.compiled.js
+++ server.compiled.js
@@ -23,7 +23,7 @@ app.all('*', function (req, res) {
     proxy.web(req, res, {
       target: 'http://111111-env.eba-9uquamkj.us-east-2.elasticbeanstalk.com/'
     }); // res.status(200)
-  } else if (req._parsedOriginalUrl.path === '/carousel') {
+  } else if (req._parsedOriginalUrl.path === '/products') {
     console.log('Carousel Request');
     proxy.web(req, res, {
       target: 'http://newcarousel-env.eba-irp2rurw.us-east-2.elasticbeanstalk.com/'
