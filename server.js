66278c432c8f93071d49555629732663f4c38634
Benjamin Polancich <polancichben@gmail.com>
2020-06-29 11:16:56 -0500

    Server Changes

--- server.js
+++ server.js
@@ -14,25 +14,25 @@ const proxy = httpProxy.createProxyServer({});
 
 
 app.all('*',(req,res)=>{
-  console.log("REQUEST MADE",req._parsedOriginalUrl.path)
-  if (req._parsedOriginalUrl.path === '/api/getReviews?id=11'){
+  let endpoint = req.params[0];
+  console.log("REQUEST MADE",endpoint[0])
+  if (endpoint === '/api/getReviews'){
     console.log('Reviews Req Made')
     proxy.web(req,res,{target:'http://111111-env.eba-9uquamkj.us-east-2.elasticbeanstalk.com/'})
     // res.status(200)
-  } else if (req._parsedOriginalUrl.path === '/products') {
+  } else if (endpoint === '/products') {
     console.log('Carousel Request')
     proxy.web(req,res,{target:'http://newcarousel-env.eba-irp2rurw.us-east-2.elasticbeanstalk.com/'})
     // res.status(200)
-  } else if (req._parsedOriginalUrl.path === '/api/get/products'){
+  } else if (endpoint === '/api/get/products'){
     console.log('SearchBar Req')
     proxy.web(req,res,{target:'http://searchbarricardo2-dev.us-east-2.elasticbeanstalk.com/'})
     // res.status(200)
-  } else if (req._parsedOriginalUrl.path === '/display'){
+  } else if (endpoint === '/display'){
     console.log('Image Display Req')
     proxy.web(req,res,{target:'http://imagecomponent-env-1.eba-4mfwjdhg.us-east-2.elasticbeanstalk.com/'})
     // res.status(200)
   } else {
-    let endpoint = req._parsedOriginalUrl.path;
     console.log('Unknown Endpoint ',endpoint)
     res.status(400).send(endpoint)
   }
